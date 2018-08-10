21ca1af4e694898ebdc0a250117c0908d42ab6fe
Austin Crane <austincrane@gmail.com>
2018-08-10 15:15:02 -0400

    add submenu support

--- inc/subnav.php
+++ inc/subnav.php
@@ -9,6 +9,18 @@
 		case 'Company':
 		wp_nav_menu( array( 'theme_location' => 'company' ) );
 		break;
+		case 'Outreach':
+		wp_nav_menu( array( 'theme_location' => 'outreach' ) );
+		break;
+		case 'Support':
+		wp_nav_menu( array( 'theme_location' => 'support' ) );
+		break;
+		case 'Dance with Us':
+		wp_nav_menu( array( 'theme_location' => 'dance' ) );
+		break;
+		case 'Performances':
+		wp_nav_menu( array( 'theme_location' => 'performances' ) );
+		break;
 		case 'None':
 		break;
 	}
