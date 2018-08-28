d760de1f015b39b453f3ac5260d2869ea49d8b68
Austin Crane <austincrane@gmail.com>
2018-08-28 11:43:34 -0400

    fix login page

--- inc/theme.php
+++ inc/theme.php
@@ -14,10 +14,13 @@ function my_login_logo() { ?>
 <style type="text/css">
   body.login div#login h1 a {
   	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
-  	background-size: 327px 67px;
-  	width: 327px;
-  	height: 67px;
+  	background-size: 300px 83px;
+  	width: 300px;
+  	height: 83px;
   }
+   body.login {
+    background-color: #fff;
+   }
 </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
